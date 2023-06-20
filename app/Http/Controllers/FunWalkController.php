e7121f30c6df8a4f1f516c1f4ab8cc806ad8c6be
OrangYangBaik <gregorykurniawan27@gmail.com>
2023-06-20 14:06:44 +0700

    controller udh semua, tapi titlenya baru dummy

---
+++ app/Http/Controllers/FunWalkController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FunWalkController extends Controller
+{
+    public function index()
+    {
+        //return view("fun-walk-landing");
+    }
+}
